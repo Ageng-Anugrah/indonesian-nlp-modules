a79e7f7f4f1ca81b6ab510a630bf4412766dadcf
Ageng Anugrah <agenganugrah@gmail.com>
2023-08-09 20:20:39 +0000

    Change req

--- NER/utils/crf_utils.py
+++ NER/utils/crf_utils.py
@@ -1,43 +1,34 @@
-def word2features(sent, i):
-    word = sent[i][0]
+def word2features_ner(df, i):
+    word = df[i][0]
     features = {
-        "word": word,
         "bias": 1.0,
+        "word.lower()": word.lower(),
+        "word.isupper()": word.isupper(),
+        "word.istitle()": word.istitle(),
+        "word.isdigit()": word.isdigit(),
     }
     if i > 0:
-        prev_word = sent[i - 1][0]
-        features.update(
-            {
-                "prev_word": prev_word,
-                "prev_word.lower()": prev_word.lower(),
-                "prev_word.isupper()": prev_word.isupper(),
-                "prev_word.istitle()": prev_word.istitle(),
-                "prev_word.isdigit()": prev_word.isdigit(),
-            }
-        )
+        prev_word = df[i-1][0]
+        features.update({
+            "-1:word.lower()": prev_word.lower(),
+            "-1:word.istitle()": prev_word.istitle(),
+            "-1:word.isupper()": prev_word.isupper(),
+        })
     else:
         features["BOS"] = True
-
-    if i < len(sent) - 1:
-        next_word = sent[i + 1][0]
-        features.update(
-            {
-                "next_word": next_word,
-                "next_word.lower()": next_word.lower(),
-                "next_word.isupper()": next_word.isupper(),
-                "next_word.istitle()": next_word.istitle(),
-                "next_word.isdigit()": next_word.isdigit(),
-            }
-        )
+    if i < len(df)-1:
+        next_word = df[i+1][0]
+        features.update({
+            "+1:word.lower()": next_word.lower(),
+            "+1:word.istitle()": next_word.istitle(),
+            "+1:word.isupper()": next_word.isupper(),
+        })
     else:
         features["EOS"] = True
-
     return features
 
-
-def sent2features(sent):
-    return [word2features(sent, i) for i in range(len(sent))]
-
+def sent2features_ner(sent):
+    return [word2features_ner(sent, i) for i in range(len(sent))]
 
 def sent2labels(sent):
     return [label for token, label in sent]
